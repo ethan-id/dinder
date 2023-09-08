ebc265192c7fd538be8c3bafb738620084c21988
newland2 <newland2@iastate.edu>
2023-09-08 16:51:32 -0500

    Experiments done I believe

--- Experiments/newland2/Experiment1/helloworld/src/main/java/coms309/WelcomeController.java
+++ Experiments/newland2/Experiment1/helloworld/src/main/java/coms309/WelcomeController.java
@@ -9,11 +9,12 @@ class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello and welcome to COMS 309 This is a cool picture for you";
+        return "Hello and welcome to COMS 309 This is a cool picture for you https://t.ly/SuOcD";
+                //<img src="cat.jpg" height="50" width="50">;
     }
 
     @GetMapping("/{name}")
     public String welcome(@PathVariable String name) {
-        return "Hello and welcome to COMS 309, this is " + name;
+        return "Hello and welcome to COMS 309 " + name;
     }
 }
