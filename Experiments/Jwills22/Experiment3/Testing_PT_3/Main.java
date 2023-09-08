1cf0ddc799e72eb8bd0a6a587a1e495db80c38c6
Jesse Williams <Jwills22@iastate.edu>
2023-09-07 19:58:50 -0500

    was trying to work with Pop ups slightly, originally used JPopUp Window to do it but switched to this and this does not work.

---
+++ Experiments/Jwills22/Experiment3/Testing_PT_3/Main.java
@@ -0,0 +1,12 @@
+package dinder;
+
+import javax.swing.*;
+
+public class Main extends JPopupMenu {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+
+    }
+}
