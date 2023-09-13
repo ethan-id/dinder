5802d2c0fb4e703df79557f4ba525cdf01a3ae74
Lalita <Lalita@iastate.edu>
2023-09-12 19:14:27 -0500

    add all experiment

---
+++ Experiments/lalikrish/Experiment1/app/src/main/java/com/example/menudisplay/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.menudisplay;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
