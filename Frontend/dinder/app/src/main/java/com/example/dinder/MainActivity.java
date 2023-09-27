5d47fa6ffe7e4248713118bdacf331538a59c920
ehancock <ehancock@iastate.edu>
2023-09-27 17:42:51 -0500

    Update login screen and remove bloat from frontend files

--- Frontend/dinder/app/src/main/java/com/example/dinder/MainActivity.java
+++ Frontend/dinder/app/src/main/java/com/example/dinder/MainActivity.java
@@ -3,54 +3,64 @@ package com.example.dinder;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.text.TextWatcher;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
-    EditText email;
+    Button loginBtn;
+    EditText username;
     EditText password;
     ImageView logo;
+
+    private JsonObjectRequest createUserLoginRequest(String username, String password) {
+        Intent intent = new Intent(MainActivity.this, CounterActivity.class);
+
+        String url = String.format("http://10.0.2.2:8080/login/%s", username);
+
+        return new JsonObjectRequest(url,
+                response -> {
+                    startActivity(intent);
+                },
+                error -> {
+                    startActivity(intent);
+                    // Handle login error/incorrect password or something
+                }
+        );
+    }
+
+    protected boolean validLogin(String username, String password) {
+        return !Objects.equals(username, "") && !Objects.equals(password, "");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        RequestQueue queue = VolleySingleton.getInstance(this.getApplicationContext()).getRequestQueue();
+
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.loginBtn);
-        email = findViewById(R.id.editTextEmail);
+        loginBtn = findViewById(R.id.loginBtn);
+        username = findViewById(R.id.editTextUsername);
         password = findViewById(R.id.editTextPassword);
         logo = findViewById(R.id.appLogo);
         logo.setImageResource(R.drawable.temporary_logo);
 
-
-        button.setEnabled(false);
-
-        // TextWatcher to observe changes in the EditText fields
-        TextWatcher textWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                // Enable button only if both email and password have content
-                button.setEnabled(!email.getText().toString().trim().isEmpty() && !password.getText().toString().trim().isEmpty());
+        // loginBtn
+        loginBtn.setOnClickListener(v -> {
+            if (validLogin(username.getText().toString(), password.getText().toString())) {
+                JsonObjectRequest loginRequest = createUserLoginRequest(username.getText().toString(), password.getText().toString());
+                queue.add(loginRequest);
             }
-        };
 
-        email.addTextChangedListener(textWatcher);
-        password.addTextChangedListener(textWatcher);
-
-        button.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, CounterActivity.class);
-            startActivity(intent);
-            email.setText("");
+            // Reset password input after clicking the button
             password.setText("");
         });
     }
