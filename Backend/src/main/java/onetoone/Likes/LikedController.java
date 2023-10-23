f9abe3b641aa8c529a646cc3e439f1a4368d3292
Jesse Williams <Jwills22@iastate.edu>
2023-10-23 17:44:37 -0500

    Added functionality to cross compare all user's likes in real time and logic for swiping

--- Backend/src/main/java/onetoone/Likes/LikedController.java
+++ Backend/src/main/java/onetoone/Likes/LikedController.java
@@ -3,14 +3,21 @@ package onetoone.Likes;
 import onetoone.Users.User;
 import onetoone.Users.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LikedController {
     @Autowired
     LikeRepository likeRepository;
+
+    @PutMapping(path="/liked")
+    public User findingwhatwasliked() {
+        User user = new User();
+        user.setNewLike(new Liked("Cornbread"));
+        user.setNewLike(new Liked("Aztecas"));
+        user.setNewLike(new Liked("Tropical Smoothie Cafe"));
+        return user;
+    }
 
 
 }
