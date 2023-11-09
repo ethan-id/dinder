06787bafd49f9391d06c1f28380f72c863eb5740
ehancock <ehancock@iastate.edu>
2023-11-09 16:29:35 -0600

    Clean up backend code

--- Backend/src/main/java/onetoone/SwaggerConfig.java
+++ Backend/src/main/java/onetoone/SwaggerConfig.java
@@ -4,13 +4,11 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableSwagger2WebMvc
 public class SwaggerConfig {
     @Bean
     public Docket api() {
