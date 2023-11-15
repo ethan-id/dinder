31b05acd3ce57c57f4eb3986d2c5df3fce5269d2
ehancock <ehancock@iastate.edu>
2023-11-15 16:14:47 -0600

    Update like_item.xml with more info

--- Frontend/dinder/app/src/androidTest/java/com/example/dinder/LoginActivityTest.java
+++ Frontend/dinder/app/src/androidTest/java/com/example/dinder/LoginActivityTest.java
@@ -27,7 +27,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
-public class LoginActivityTest {
+public class LoginActivityTest {w
     private VolleyIdlingResource idlingResource;
 
     @Before
